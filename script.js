4ff3d6c399c8184010e9b4948b563e15f2114231
Mateo <mateofontaine7@gmail.com>
2024-07-28 18:02:31 -0300

    Agregamos el acordion a la seccion de property

--- script.js
+++ script.js
@@ -69,3 +69,20 @@ dropdowns.forEach(dropdown => {
         }
     });
 });
+
+
+const faqsTitles = document.querySelectorAll(".faq .head");
+
+faqsTitles.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.parentNode.classList.toggle("active");
+        
+        const faqContent = faq.nextElementSibling;
+
+        if (faq.parentNode.classList.contains("active")) {
+            faqContent.style.height = (faqContent.scrollHeight + 30) + "px";
+        } else {
+            faqContent.style.height = "0px";
+        }
+    });
+});
